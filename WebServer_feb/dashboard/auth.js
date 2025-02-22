f3081ed42af8fd123cd9f491267b86fa95d9a4a8
Vincey0423 <168004144+Vincey0423@users.noreply.github.com>
2025-02-23 01:04:58 +0800

    Untrack all files

--- WebServer_feb/dashboard/auth.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://wcdscmlpgtstfwuagutt.supabase.co";  // Replace with your actual Supabase URL
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZHNjbWxwZ3RzdGZ3dWFndXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMjE2NDAsImV4cCI6MjA1MzY5NzY0MH0.BrYUgkRLDsx0lOccC0x2IlNsi0RzoMDgu0SfgJCCuwI";  // Replace with your actual Supabase anon key
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
