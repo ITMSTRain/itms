8bf3eef6b68c9e542b93a3b48bb2d3f4809a1584
ITMSTRain <>
2025-02-25 00:09:42 +0800

    minor changes

--- next_frontend/app/page.tsx
+++ next_frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Landing() {
     <main className="flex h-screen items-center justify-center bg-gray-900">
       <button 
         type="button" 
-        onClick={() => router.push('/home')}
+        onClick={() => router.push('/login')}
         className="px-6 py-3 text-lg font-bold text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-all"
       >
         Home
