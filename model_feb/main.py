fix: Update Supabase environment variables and adjust allowed origins for local development refactor: Clean up CUDA availability checks in YOLO model loading fix: Correct speed calculation logic in SpeedCalculator class fix: Update About page title for correct spelling fix: Comment out logo image paths in header components for future reference refactor: Update Next.js configuration for static export and allowed origins

model_feb/main.py
@@ -67,18 +67,21 @@ app.add_middleware(
 # Load the YOLO model
 model = YOLO(r'..\model_feb\94%.pt')
 
-# Check for CUDA availability
-if not torch.cuda.is_available():
-    print("CUDA is not available.")
-    device = model.device
-    print(f"🔥 YOLO is running on: {device}")    
+device = model.device
+print(f"YOLO is running on: {device}")
+
+# # Check for CUDA availability
+# if not torch.cuda.is_available():
+#     print("CUDA is not available.")
+#     device = model.device
+#     print(f"YOLO is running on: {device}")    
    
 
-else:
-    print("CUDA is available.")
-    device = 'cuda'
-    model.to(device)
-    print(torch.cuda.get_device_properties(0))
+# else:
+#     print("CUDA is available.")
+#     device = 'cuda'
+#     model.to(device)
+#     print(torch.cuda.get_device_properties(0))
 
     
 